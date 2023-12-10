40843213b85f7b5a7168600780b1e4cddadac6ec
Gulnar2003 <146223070+Gulnar2003@users.noreply.github.com>
2023-12-10 19:42:21 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,87 +1,53 @@
-  import React, {useEffect, useState } from 'react';
-  import MovieList from './components/MovieList';
-  import './App.css';
-  import MovieListHeading from './components/MovieListHeading';
-  import SearchBox from './components/SearchBox';
-  import AddFavourites from './components/AddFavourites';
-  import RemoveFavourites from './components/RemoveFavourites';
-
-  const App = () => {
-    const [movies, setMovies]= useState([]);
-    const [favourites , setFavourites] = useState([]);
-    const [searchValue , setSearchValue] = useState('');
-
-    const getMovieRequest = async (searchValue) => {
-      const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=3636380a`;
-      const response = await fetch(url);
-      const responseJson = await response.json();
-
-      if(responseJson.Search){
-        setMovies(responseJson.Search);
-      }
-    };
-
-    useEffect(() =>{
-      getMovieRequest(searchValue);
-    }, [searchValue]);
-
-    useEffect(() =>{
-      const movieFavourites = JSON.parse(
-        localStorage.getItem('react-my-app-favourites')
-        );
-        setFavourites(movieFavourites);
-    }, []);
-
-    const saveToLocalStorage = (items) => {
-  localStorage.setItem('react-my-app-favourites', JSON.stringify(items));
-    }
 
 
-    const addFavouriteMovie = (movie) => {
-      const existingMovie = favourites.find((favMovie) => favMovie.imdbID === movie.imdbID);
-    
-      if (!existingMovie) {
-        const newFavouriteList = [...favourites, movie];
-        setFavourites(newFavouriteList);
-        saveToLocalStorage(newFavouriteList);
-      } else {
-        alert('This movie is already in your favourites!');
-      }
-    };    
+import React from 'react';
+import { Route, Routes, Link, BrowserRouter } from 'react-router-dom';
+import Home from './pages/Home.jsx'
+import List from './pages/List.jsx'
+import Detail from './pages/Detail.jsx'
 
-    const removeFavouriteMovie = (movie) => {
-      const newFavouriteList = favourites.filter(
-        (favourite) => favourite.imdbID !== movie.imdbID 
-        );
-        setFavourites(newFavouriteList);
-        saveToLocalStorage(newFavouriteList);
-    };
 
+import './index.css'
+// import './reset.css';
+
+class App extends React.Component {
+  render() {
     return (
-    <div className='container-fluid movie-app'>
-      <div className='row '>
-        <MovieListHeading heading='Movies'/>
-        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
-      </div>
-      <div className='row'>
-      <MovieList 
-      movies={movies} 
-      handleFavouritesClick={addFavouriteMovie}
-      favouriteComponent={AddFavourites} />
-      </div>
+<BrowserRouter>
+        <div>
+          <nav className="navbar">
+            <div className="collapse" id="navbarNav">
 
-      <div className='row '>
-        <MovieListHeading heading='Favourites' />
-      </div>
-      <div className='row'>
-      <MovieList 
-      movies={favourites} 
-      handleFavouritesClick={removeFavouriteMovie}
-      favouriteComponent={RemoveFavourites} 
-      />
-      </div>
-    </div>
-  );
-  };
+              <ul className="navbar-nav">
+                <li className="nav-item">
 
-  export default App;
+                  <Link className="nav-link" to="/Home">
+                    Home
+                  </Link>
+
+                </li>
+                <li className="nav-item">
+
+                  <Link className="nav-link" to="/List">
+                    List
+                  </Link>
+
+                </li>
+              </ul>
+
+            </div>
+          </nav>
+         <Routes>
+            <Route path="/Home" element={<Home/>}/>
+            <Route path="/List" element={<List/>}/>
+            <Route path="/Detail" element={<Detail/>}/>
+         </Routes>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
+      
+   
